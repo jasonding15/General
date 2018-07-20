3f542e575fa9364b144857843dd8c6775b279c22
Jason Ding <Jason Ding@DESKTOP-6CQ6DCB>
2018-07-20 19:02:36 -0400

    push with finished road

--- Ding, Jason/src/stopLightsAtIntersection/LightsRunnerOnRoad.java
+++ Ding, Jason/src/stopLightsAtIntersection/LightsRunnerOnRoad.java
@@ -18,14 +18,26 @@ public class LightsRunnerOnRoad extends PApplet
 	
 	public void setup()
 	{
-		stoplight = new LightAtIntersection(this, 50, 50, 3, false);
+		stoplight = new LightAtIntersection(this, 50, 50, 1, true);
 	}
 	
 	public void draw()
 	{
 		background(150, 190, 255);
+
 		fill(0);
-		rect(400, 0, 200, 800);
+		rect(400, 0, 200, 800); //road
+		rect(0, 300, 1000, 200); //road
+		fill(255, 250, 0); // yellow lines
+		rect(0, 394, 400, 6);
+		rect(0, 401, 400, 6);
+		rect(600, 394, 400, 6);
+		rect(600, 401, 400, 6);
+		rect(494, 0, 6, 300);
+		rect(501, 0, 6, 300);
+		rect(494, 500, 6, 300);
+		rect(501, 500, 6, 300);
+
 		stoplight.drawSelf();
 	}
 
