c5fedc44bce4fbfcea3fe2b969b748022c9fb36d
jding <jding@10.10.122.42>
2018-07-30 08:22:02 -0400

    Done

--- Ding, Jason/src/craps/GameRunner.java
+++ Ding, Jason/src/craps/GameRunner.java
@@ -10,41 +10,6 @@ public class GameRunner
 		test.printInstructions();
 		test.setTotalMoney();
 		test.playUntilQuit();
-//		test.playOneRound();
-//		test.placeBet();
-//		test.rollDice();
-		
-		
-
-//		Game test = new Game();
-//		test.placeBet(50);
-//		System.out.print(test.rollDie() + "  ");
-//		System.out.println("Game over? " + test.checkIfGameEnds());
-////		System.out.println(test.hasWon());
-////		System.out.println(test.getPoint());
-//		while (test.checkIfGameEnds() == false)
-//		{
-//			System.out.println(test.rollDie() + "  " + test.hasWon());
-//		}
-//		System.out.println("Game over! Did you win? " + test.hasWon());
-//		test.resolveBets();
-//		System.out.println("you now have: " + test.getTotalMoney());
-//		
-//		
-//		
-//		test.resetGame();
-//		test.placeBet(50);
-//		System.out.println(test.rollDie());
-//		System.out.println(test.checkIfGameEnds());
-//		while (test.checkIfGameEnds() == false)
-//		{
-//			
-//			System.out.print(test.rollDie() + "  ");
-//			System.out.println(test.hasWon() + "  ");
-//		}
-//		System.out.println(test.hasWon() + " ");
-//		test.resolveBets();
-//		System.out.println("You now have: " + test.getTotalMoney());
 	}
 
 }
