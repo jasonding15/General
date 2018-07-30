17df441fa4b713edf28b6ee6d1bc51069a892237
jding <jding@10.10.122.42>
2018-07-30 08:15:19 -0400

    finished some stuff

--- Ding, Jason/src/programmingChallengesCh7/ChargeAccountValidationTester.java
+++ Ding, Jason/src/programmingChallengesCh7/ChargeAccountValidationTester.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 
 public class ChargeAccountValidationTester
 {
-
+	// create the array with all the correct answers
+	// take input from user of a number
+	// loop through the array with a binary sort, checking if the users number matches a number in the array
+	// if it matches, return true, else return false
+	
 	public static void main(String[] args) 
 	{
 		int[] arr = {5658845, 8080152, 1005231, 4520125, 4562555, 6545231, 7895122, 5552012, 3852085, 8777541, 5050552, 7576651, 845127, 7825877, 788120, 1302850, 1250255, 4581002};
