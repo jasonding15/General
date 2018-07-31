70bc14a7b35d257ab862f6346462e4dd4a14d0d5
jding <jding@10.10.122.42>
2018-07-31 10:06:14 -0400

    done with this

--- Ding, Jason/src/programmingChallengesCh7/DriverLisenceExam.java
+++ Ding, Jason/src/programmingChallengesCh7/DriverLisenceExam.java
@@ -1,10 +1,30 @@
 package programmingChallengesCh7;
 
-public class DriverLisenceExam {
+import java.util.Scanner;
+
+public class DriverLisenceExam 
+{
 
 	public static void main(String[] args) 
 	{
+		Scanner fromKeyboard = new Scanner(System.in);
+		String[] answer = new String[20];
+		for (int i = 1; i <= answer.length; i++)
+		{
+			System.out.print("What is the answer to question " + i + "? ('A', 'B', 'C', or 'D'): ");
+			String ans = fromKeyboard.nextLine();
+			answer[i - 1] = ans;
+		}
+		fromKeyboard.close();
+		DriversLicense dl = new DriversLicense(answer);
 		
+		System.out.println("You got " + dl.totalCorrect() + " questions right.");
+		System.out.println("You got " + dl.totalIncorrect() + " questions wrong.");
+		System.out.println("You got these questions wrong: " + dl.gotWrong());
+		if (dl.didPass())
+			System.out.println("You passed, congradulations!");
+		else 
+			System.out.println("Sorry, you did not pass.");
 	}
 
 }
--- Ding, Jason/src/programmingChallengesCh7/DriversLicense.java
+++ Ding, Jason/src/programmingChallengesCh7/DriversLicense.java
@@ -1,27 +1,56 @@
 package programmingChallengesCh7;
 
+import java.util.ArrayList;
+
+/*
+ * What is you answer to number (1 through 20)?:A
+ * You got 3 questions wrong.
+ * You got 17 questions right.
+ * You passed!
+ */
 public class DriversLicense 
 {
 	private String[] quiz;
 	private String[] scores;
-	public DriversLicense(quz)
+	
+	public DriversLicense(String[] quz)
 	{
-		String[] scores = {"A", "B", "C", "D", "D", "C", "B", "C", "A", "D","A", "B", "C", "D", "D", "C", "B", "C", "A", "D"};
-		quiz = 
+		scores = new String[]{"A", "A", "A", "A", "A", "A", "A", "A", "A", "D","A", "B", "C", "D", "D", "C", "B", "C", "A", "D"};
+		quiz = quz;
 	}
 	
 	public int totalCorrect()
 	{
 		int totalCorrect = 0;
-		for (int i = 0; i < scores.length(); i++)
+		for (int i = 0; i < scores.length; i++)
 		{
-			if (quiz[i] == scores[i])
+			if (quiz[i].equals(scores[i]))
 				totalCorrect++;
-			
-				
 		}
+		return totalCorrect;
 	}
 	
+	public int totalIncorrect()
+	{
+		return scores.length - totalCorrect();
+	}
 	
+	public boolean didPass()
+	{
+		if (totalCorrect() >= 15)
+			return true;
+		return false;
+	}
+	
+	public ArrayList<Integer> gotWrong()
+	{
+		ArrayList<Integer> wrongOnes = new ArrayList<Integer>();
+		for (int i = 0; i < scores.length; i++)
+		{
+			if (! scores[i].equals(quiz[i]) )
+				wrongOnes.add(i + 1);
+		}
+		return wrongOnes;
+	}
 	
 }
