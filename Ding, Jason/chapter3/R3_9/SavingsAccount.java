4aae73756fee778b55f9fb537b54c86af91527d9
jding <jding@10.10.122.152>
2018-09-17 11:00:23 -0400

    update

--- Ding, Jason/chapter3/R3_9/SavingsAccount.java
+++ Ding, Jason/chapter3/R3_9/SavingsAccount.java
@@ -2,5 +2,31 @@ package R3_9;
 
 public class SavingsAccount 
 {
+	/**
+     * allows user to deposit money into account
+     * @param mon the amount of money the user wants to deposit
+     */
+	public void depositMoney(double mon)
+	{
+		
+	}
+	
+	/**
+     * allows user to view amount of money in account
+     * @return the amount of money in account
+     */
+	public double getBalance()
+	{
+		return -1.0;
+	}
+	
+	/**
+     * allows user to withdraw money out of the account
+     * @return the amount of money left in the account
+     */
+	public double withdraw()
+	{
+		return -1.0;
+	}
 	
 }
