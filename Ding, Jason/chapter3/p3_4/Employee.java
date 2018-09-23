29b38884fd4423962e555c570ff11d8435195335
jasonding15 <jason.l.ding@gmail.com>
2018-09-23 18:51:42 -0400

    finished chapter 3 hw

--- Ding, Jason/chapter3/p3_4/Employee.java
+++ Ding, Jason/chapter3/p3_4/Employee.java
@@ -1,23 +1,46 @@
 package p3_4;
 
+/**
+ * able to keep track of employees name and manage his salary
+ * @author Jason Ding
+ *
+ */
 public class Employee 
 {
 	private String employeeName;
 	private double salary;
+	
+	/**
+	 * creates a new tab on an employee
+	 * @param employeeName is the name of the employee
+	 * @param salary is the yearly income of the employee
+	 */
 	public Employee(String employeeName, double salary)
 	{
 		this.employeeName = employeeName;
 		this.salary = salary;
 	}
+	
+	/**
+	 * @return the employee's name
+	 */
 	public String getName()
 	{
 		return employeeName;
 	}
+	
+	/**
+	 * @return the employee's salary
+	 */
 	public double getSalary()
 	{
 		return salary;
 	}
 	
+	/**
+	 * gives the employee a raise
+	 * @param byPercent percentage to raise the employee's annual income
+	 */
 	public void raiseSalary(double byPercent)
 	{
 		salary += salary * byPercent / 100.0;
--- Ding, Jason/chapter3/p3_5/Car.java
+++ Ding, Jason/chapter3/p3_5/Car.java
@@ -1,24 +1,43 @@
 package p3_5;
 
+/**
+ * keeps track of the amount of gas in the car
+ * @author Jason Ding
+ */
 public class Car 
 {
 	private double mpg, gasInTank;
 	
+	/**
+	 * creates a new tab on a car
+	 * @param mpg is the amount of miles the car can travel per gallon of gas
+	 */
 	public Car(double mpg)
 	{
 		this.mpg = mpg;
 	}
 	
+	/**
+	 * @return the amoun of gas in the tank
+	 */
 	public double getGasInTank()
 	{
 		return gasInTank;
 	}
 	
+	/**
+	 * adds gas to tank
+	 * @param gas is the amount of gas added to the tank
+	 */
 	public void addGas(double gas)
 	{
 		gasInTank += gas;
 	}
 	
+	/**
+	 * calculates the gas left after driving a set amount of miles
+	 * @param miles is the amount of miles the car traveled
+	 */
 	public void drive(double miles)
 	{
 		gasInTank -= miles / mpg;
