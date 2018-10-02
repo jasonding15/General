cccedda32a3e44f31ba0bd4e6030fd1337bb8b2a
jding <jding@10.10.122.140>
2018-10-02 10:59:52 -0400

    final commit

--- Ding, Jason/chapter4/project4_14/Triangle.java
+++ Ding, Jason/chapter4/project4_14/Triangle.java
@@ -4,6 +4,8 @@ import java.awt.geom.Point2D;
 
 public class Triangle 
 {
+	private Point2D.Double p1, p2, p3;
+	
 	/**
 	 * A triangle with three sides and three vertices.
 	 * Each vertex has a location in the coordinate plane and an angle.
@@ -15,7 +17,9 @@ public class Triangle
 	 */
 	public Triangle( Point2D.Double p1, Point2D.Double p2, Point2D.Double p3 )
 	{
-		
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
 	}
 	
 	/**
