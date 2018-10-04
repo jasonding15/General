65ecad87485ed48a7a4391d35ea83f59edec183d
jding <jding@10.10.122.140>
2018-10-04 10:59:50 -0400

    worked a bit

--- Ding, Jason/chapter4/p4_14/TimeInterval.java
+++ Ding, Jason/chapter4/p4_14/TimeInterval.java
@@ -2,5 +2,20 @@ package p4_14;
 
 public class TimeInterval 
 {
+	private int time1, time2;
+	public TimeInterval(int time1, int time2)
+	{
+		this.time1 = time1;
+		this.time2 = time2;
+	}
 	
+	public getHours()
+	{
+		
+	}
+	
+	public getMinutes()
+	{
+		
+	}
 }
