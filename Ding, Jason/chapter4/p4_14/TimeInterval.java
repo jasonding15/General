35656e38445044ca6bb571d62aa880b4ab399617
jding <jding@10.10.122.140>
2018-10-05 10:20:49 -0400

    worked on this

--- Ding, Jason/chapter4/p4_14/TimeInterval.java
+++ Ding, Jason/chapter4/p4_14/TimeInterval.java
@@ -2,20 +2,40 @@ package p4_14;
 
 public class TimeInterval 
 {
-	private int time1, time2;
-	public TimeInterval(int time1, int time2)
+	private String time1, time2;
+	
+	/**
+	 * returns the time between two military times
+	 * @param time1 the first time
+	 * @param time2 the latter time
+	 */
+	public TimeInterval(String time1, String time2)
 	{
 		this.time1 = time1;
 		this.time2 = time2;
 	}
 	
-	public getHours()
+	public int getHours()
 	{
-		
+		return -1;
 	}
 	
-	public getMinutes()
+	public int getMinutes()
+	{
+		return -1;
+	}
+	
+	private int getTotalTime()
 	{
 		
+		return -1;
+	}
+	
+	private int convertToMinutes(String time)
+	{
+		int hours = Integer.parseInt(time.substring(0, 2));
+		int minutes = Integer.parseInt(time.substring(2,4));
+		hours *= 60;
+		return hours + minutes;
 	}
 }
---
+++ Ding, Jason/chapter4/p4_14/TimeIntervalTester.java
@@ -0,0 +1,12 @@
+package p4_14;
+
+public class TimeIntervalTester 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
