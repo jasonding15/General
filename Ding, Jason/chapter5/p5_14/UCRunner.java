82676bdb2fcfcf4f5e1a5b0e332a1bf549d17fb7
jding <jding@10.10.122.158>
2018-10-17 10:59:38 -0400

    started

---
+++ Ding, Jason/chapter5/p5_14/UCRunner.java
@@ -0,0 +1,12 @@
+package p5_14;
+
+public class UCRunner 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Ding, Jason/chapter5/p5_14/UnitConverter.java
+++ Ding, Jason/chapter5/p5_14/UnitConverter.java
@@ -2,5 +2,18 @@ package p5_14;
 
 public class UnitConverter 
 {
-
+	public UnitConverter()
+	{
+		
+	}
+	
+	public toMeters()
+	{
+		
+	}
+	
+	public fromMeters()
+	{
+		
+	}
 }
