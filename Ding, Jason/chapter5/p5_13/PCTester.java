9ecdc51d1caa058a51d33f1a2a6a032a11d2f8e1
jasonding15 <jason.l.ding@gmail.com>
2018-10-17 23:11:48 -0400

    finished? I think so!

---
+++ Ding, Jason/chapter5/p5_13/PCTester.java
@@ -0,0 +1,12 @@
+package p5_13;
+
+public class PCTester 
+{
+
+	public static void main(String[] args) 
+	{
+		PayCheck pc = new PayCheck(10, 39);
+		System.out.println(pc.getPay());
+	}
+
+}
--- Ding, Jason/chapter5/p5_13/PayCheck.java
+++ Ding, Jason/chapter5/p5_13/PayCheck.java
@@ -2,5 +2,27 @@ package p5_13;
 
 public class PayCheck 
 {
-
+	private double hourlyWage;
+	private double hoursWorked;
+	
+	public PayCheck(double wage, double work) 
+	{
+		hourlyWage = wage;
+		hoursWorked = work;
+	}
+	
+	public double getPay() 
+	{
+		if(hoursWorked > 40) 
+		{
+			double overtime = hoursWorked - 40;
+			double paycheck = (overtime * hourlyWage) * 1.5;
+			paycheck = paycheck + ((hoursWorked - overtime) * hourlyWage);
+			return paycheck;
+		}
+		else
+			return (hourlyWage * hoursWorked);
+	}
 }
+
+
