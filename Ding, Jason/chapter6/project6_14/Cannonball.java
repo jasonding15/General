8818aead35c4362f1af4e7d8f97819d928bfa14c
jasonding15 <jason.l.ding@gmail.com>
2018-10-26 16:21:24 -0400

    initial push

---
+++ Ding, Jason/chapter6/project6_14/Cannonball.java
@@ -0,0 +1,46 @@
+package project6_14;
+
+public class Cannonball 
+{
+	private static final double DELTAT = 0.01;
+	private static final double GRAVITY = 9.81;
+	private double hVelocity, vVelocity, distance, height;
+	private int time;
+	public Cannonball(double velocity, double angle)
+	{
+		hVelocity = Math.cos(angle) * velocity;
+		vVelocity = Math.cos(angle) * velocity;
+		height = 0;
+		distance = 0;
+		time = 0;
+	}
+	
+	public void updatePosition()
+	{
+		height = height + vVelocity * DELTAT;
+		vVelocity = vVelocity - GRAVITY * DELTAT;
+		distance += hVelocity * DELTAT;
+		time++;
+		if(time % 100 == 0 || height < 0)
+		{
+			if (height <= 0)
+				height = 0;
+			System.out.println("Time: " + time / 100.0);
+			System.out.println("Distance: " + distance);
+			System.out.println("Altitude: " + height);
+			System.out.println();	
+		}
+				
+	}
+	
+	public double getHeight()
+	{
+		return height;
+	}
+	
+	public double getDistance()
+	{
+		return distance;
+	}
+	
+}
---
+++ Ding, Jason/chapter6/project6_14/CannonballLauncher.java
@@ -0,0 +1,24 @@
+package project6_14;
+
+import java.util.Scanner;
+
+public class CannonballLauncher 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input velocity: ");
+		double velocity = input.nextDouble();
+		System.out.print("Input angle(degrees): ");
+		double angle = input.nextDouble();
+		input.close();
+		Cannonball cball = new Cannonball(velocity, Math.toRadians(angle));
+		cball.updatePosition();
+		while(cball.getHeight() > 0)
+			cball.updatePosition();
+//		if (cball.getHeight() <= 0)
+//			cball.updatePosition();
+	}
+
+}
