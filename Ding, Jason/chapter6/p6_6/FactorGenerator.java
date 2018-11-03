cf390c8c73fb06303faee6aa3856dcf5bd8dba48
jasonding15 <jason.l.ding@gmail.com>
2018-11-03 09:46:37 -0400

    finished factor generator

--- Ding, Jason/chapter6/p6_6/FactorGenerator.java
+++ Ding, Jason/chapter6/p6_6/FactorGenerator.java
@@ -2,18 +2,53 @@ package p6_6;
 
 public class FactorGenerator 
 {
+	private int numberToFactor, nextFactor;
 	public FactorGenerator(int numberToFactor)
 	{
-		
+		this.numberToFactor = numberToFactor;
+		nextFactor = 2;
 	}
 	
+	/**
+	 * sets the numberToFactor to it divided by nextFactor
+	 * @return the number that was divided into the numberToFactor
+	 */
 	public int nextFactor()
 	{
-		
+		boolean canFactor = hasMoreFactors();
+		if (canFactor)
+		{
+			numberToFactor /= nextFactor;
+			return nextFactor;
+		}
+		return -1;
 	}
 	
+	/**
+	 * determines if the numberToFactor has more prime factors
+	 * sets nextFactor to the next prime factor
+	 * @return if the numberToFactor has more prime factors
+	 */
 	public boolean hasMoreFactors()
 	{
-		
+		while(nextFactor <= numberToFactor)
+		{
+			if (isPrime(nextFactor) && numberToFactor % nextFactor == 0)
+				return true;
+			nextFactor ++;
+		}
+		return false;
+	}
+	
+	private boolean isPrime(int x)
+	{
+		if (x == 2)
+			return true;
+		for (int i = 2; i < x; i++)
+		{
+			if (x % i == 0)
+				return false;
+		}
+		return true;
 	}
 }
--- Ding, Jason/chapter6/p6_6/FactorPrinter.java
+++ Ding, Jason/chapter6/p6_6/FactorPrinter.java
@@ -1,11 +1,25 @@
 package p6_6;
 
+import java.util.Scanner;
+
 public class FactorPrinter 
 {
 
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input number: ");
+		FactorGenerator fg = new FactorGenerator(input.nextInt());
+		int nextPrime = fg.nextFactor();
+		if (nextPrime == -1)
+			System.out.println("Number is prime. ");
+		while(nextPrime >= 0)
+		{
+			System.out.println(nextPrime);
+			nextPrime = fg.nextFactor();
+		}
 		
+		input.close();
 	}
 
 }
