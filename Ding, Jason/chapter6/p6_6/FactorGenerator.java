f39a639a81a1ff5ccbb4f03517576a44f1c97897
jasonding15 <jason.l.ding@gmail.com>
2018-11-03 17:54:05 -0400

    initial

---
+++ Ding, Jason/chapter6/p6_6/FactorGenerator.java
@@ -0,0 +1,19 @@
+package p6_6;
+
+public class FactorGenerator 
+{
+	public FactorGenerator(int numberToFactor)
+	{
+		
+	}
+	
+	public int nextFactor()
+	{
+		
+	}
+	
+	public boolean hasMoreFactors()
+	{
+		
+	}
+}
---
+++ Ding, Jason/chapter6/p6_6/FactorPrinter.java
@@ -0,0 +1,11 @@
+package p6_6;
+
+public class FactorPrinter 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
