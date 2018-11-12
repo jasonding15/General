328ae6b236fca62b93d5a08f97cd6c5d6c5f935f
jding <jding@10.10.122.146>
2018-11-12 10:59:21 -0500

    intial

--- Ding, Jason/chapter7/p7_2to6/Purse.java
+++ Ding, Jason/chapter7/p7_2to6/Purse.java
@@ -1,6 +1,17 @@
 package p7_2to6;
 
+import java.util.ArrayList;
+
 public class Purse 
 {
-
+	private ArrayList<String> arr;
+	public Purse()
+	{
+		arr = new ArrayList<String>();
+	}
+	
+	public void addCoin(String coinName)
+	{
+		
+	}
 }
--- Ding, Jason/chapter7/p7_2to6/PurseTester.java
+++ Ding, Jason/chapter7/p7_2to6/PurseTester.java
@@ -2,10 +2,8 @@ package p7_2to6;
 
 public class PurseTester 
 {
-
 	public static void main(String[] args) 
 	{
-		
 	}
 
 }
