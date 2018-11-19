09ecb0640bcdc959449683077bdb2c11b914983d
jding <jding@10.10.122.159>
2018-11-19 10:59:11 -0500

    have yet to test transfer method

--- Ding, Jason/chapter7/p7_2to6/Purse.java
+++ Ding, Jason/chapter7/p7_2to6/Purse.java
@@ -12,6 +12,31 @@ public class Purse
 	
 	public void addCoin(String coinName)
 	{
+		arr.add(coinName);
+	}
+	
+	public void reverseCoins()
+	{
+		for (int i = 0; i < arr.size() / 2; i++)
+		{
+			String temp = arr.get(i);
+			arr.set(i, arr.get(arr.size() - i - 1));
+			arr.set(arr.size() - 1 - i, temp);
+		}
+	}
+	
+	public void transferPurse(Purse a, Purse b)
+	{
+		for (int i = 0; i < b.arr.size(); i++)
+		{
+			a.addCoin(b.arr.get(i));
+		}
+		b.arr.clear();
 		
+	}
+	
+	public String toString()
+	{
+		return arr.toString(); 
 	}
 }
--- Ding, Jason/chapter7/p7_2to6/PurseTester.java
+++ Ding, Jason/chapter7/p7_2to6/PurseTester.java
@@ -4,7 +4,14 @@ public class PurseTester
 {
 	public static void main(String[] args) 
 	{
-		
+		Purse p = new Purse();
+		p.addCoin("Quarter");
+		p.addCoin("Nickel");
+		p.addCoin("Penny");
+		p.addCoin("Dime");
+		System.out.println(p.toString());
+		p.reverseCoins();
+		System.out.println(p.toString());
 	}
 
 }
