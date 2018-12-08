855fd7c5bdd836394ae9002aa2b5de9ce60f973b
jasonding15 <jason.l.ding@gmail.com>
2018-12-08 10:58:49 -0500

    finished magic square in quite possibly the worst way possible

--- Ding, Jason/chapter7/p7_13/MSRunner.java
+++ Ding, Jason/chapter7/p7_13/MSRunner.java
@@ -1,11 +1,17 @@
 package p7_13;
 
+import java.util.Arrays;
+
 public class MSRunner 
 {
 
 	public static void main(String[] args) 
 	{
-		
+		MagicSquare ms = new MagicSquare(5);
+		for(int i = 0; i < ms.getGrid().length; i++)
+		{
+			System.out.println(Arrays.toString(ms.getGrid()[i]));
+		}
 	}
 
 }
--- Ding, Jason/chapter7/p7_13/MagicSquare.java
+++ Ding, Jason/chapter7/p7_13/MagicSquare.java
@@ -2,17 +2,49 @@ package p7_13;
 
 public class MagicSquare 
 {
-	private int side;
-	public MagicSquare(int n)
+	private int[][] grid;
+	public MagicSquare(int side)
 	{
-		side = n;
+		grid = new int[side][side];
+		int index = 0;
+		int row = side - 2;
+		int col = side / 2 - 1;
+		for( int i = 0; i < side * side; i++)
+		{
+			if(row == side - 1 && col == side - 1)
+			{
+				row = side - 2;
+				col = side - 1;
+			}
+			else
+			{
+				if(row < side - 1)
+					row++;
+				else
+					row = 0;
+				if(col < side - 1)
+					col++;
+				else
+					col = 0;
+			}
+			if (grid[row][col] != 0)
+			{
+				row -= 2;
+				col--;
+				if(row >= side || row < 0)
+					row = 0;
+				if(col >= side || col < 0)
+					col = 0;
+			}
+			index ++;
+			grid[row][col] = index;
+		}
 	}
 	
-	public int[][] constructSquare()
+	public int[][] getGrid()
 	{
-		int[][] grid = new int[side][side];
-		int index = 1;
-		grid[side][side / 2 + 1] = index;
 		return grid;
 	}
+	
+	
 }
