7c6e5fa54b935909d2d084028c3b30fce83e3744
jding <jding@10.10.122.191>
2019-01-04 10:58:49 -0500

    not done w 9.9 yet

---
+++ Ding, Jason/chapter8/p9_9/DSTester.java
@@ -0,0 +1,15 @@
+package p9_9;
+
+public class DSTester 
+{
+
+	public static void main(String[] args) 
+	{
+		String str1 = "hello there";
+		DataSet ds = new DataSet(str1);
+		String str2 = "what is up";
+		String str3 = "the sun! haha";
+		
+	}
+
+}
---
+++ Ding, Jason/chapter8/p9_9/DataSet.java
@@ -0,0 +1,44 @@
+package p9_9;
+
+/**
+ * Computes the average of a set of data values.
+ */
+public class DataSet
+{
+	private Comparable maximum, minimum;
+	/**
+	 * Constructs an data set with one (the first) element.
+	 */
+	public DataSet(Comparable x)
+	{
+		maximum = x;
+		minimum = x;
+	}
+
+	/**
+	 * Adds a data value to the data set.
+	 * 
+	 * @param x
+	 *            a data value
+	 */
+	public void add(Comparable x)
+	{
+		if (maximum.compareTo(x) < 0)
+			maximum = x;
+		if (minimum.compareTo(x) > 0)
+			minimum = x;
+	}
+
+	/**
+	 * Gets the largest of the added data.
+	 */
+	public Comparable getMaximum()
+	{
+		return maximum;
+	}
+	
+	public Comparable getMinimum()
+	{
+		return minimum;
+	}
+}
