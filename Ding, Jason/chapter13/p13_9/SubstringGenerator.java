630536d1b123f4c902db66535cda11b3addb237d
jding <jding@10.10.122.156>
2019-02-06 10:37:50 -0500

    initial

---
+++ Ding, Jason/chapter13/p13_9/SubstringGenerator.java
@@ -0,0 +1,20 @@
+package p13_9;
+
+import java.util.ArrayList;
+
+public class SubstringGenerator
+{
+	public static void main(String[] args)
+	{
+		
+	}
+
+	public static ArrayList<String> getSubstrings(String x)
+	{
+		ArrayList<String> arr = new ArrayList<String>();
+		if (x.length() == 0)
+			arr.add("");
+		
+		
+	}
+}
