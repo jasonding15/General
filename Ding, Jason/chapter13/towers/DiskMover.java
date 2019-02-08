e0679e98895f5462322f8e591852076b2dadea34
jding <jding@10.10.122.156>
2019-02-08 10:58:26 -0500

    initial

---
+++ Ding, Jason/chapter13/towers/DiskMover.java
@@ -0,0 +1,30 @@
+package towers;
+
+public class DiskMover
+{
+	public static void main(String[] args)
+	{
+		move(3, 1, 3);
+	}
+	
+    public static void move(int disks, int source, int target)
+    {
+        if (disks == 1)
+        {
+        		System.out.println("Move peg " + source + " to peg " + target);
+        		return;
+        }
+        
+        int nonPeg = 0;
+        if (source + target == 3)
+        		nonPeg = 3;
+        else
+        		nonPeg = Math.abs(target - source);
+        
+        System.out.println("Move peg " + source + " to peg " + target);
+
+        move(disks--, source, nonPeg);
+        move(1, source, nonPeg);
+        move(disks--, nonPeg, target);
+    }
+}
