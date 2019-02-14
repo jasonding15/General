a575d5da15a19392b5a5c55c0ba2547e6e6f7207
jding <jding@10.10.122.144>
2019-02-14 10:57:44 -0500

    initial

--- Ding, Jason/chapter13/directoryTraversal/DirectoryTraversal.java
+++ Ding, Jason/chapter13/directoryTraversal/DirectoryTraversal.java
@@ -6,11 +6,28 @@ public class DirectoryTraversal
 {
     public static void main(String[] args)
     {
-        
+        File f = new File("/Users/jding/Documents");
+    		printFileNames(f, 8);
     }
     
     public static void printFileNames(File f, int maxLevels)
     {
+        if(maxLevels == 0)
+        {
+        		System.out.println(f.getAbsolutePath());
+        		return;
+        }
+        if (f.isDirectory())
+        {
+//        		File[] arr = f.listFiles();
+        		for(File a : f.listFiles())
+        		{
+        			printFileNames(a, maxLevels - 1);
+        		}		
+        }
+        else
+        		printFileNames(f, maxLevels - 1);
         
+        // getAbsolutePath = returns file path in string, isDirectory = boolean if folder, listFiles = array of files in folder
     }
 }
