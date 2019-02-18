efd4beb2a2dd75738b97bccd34b00195c647b996
jasonding15 <jason.l.ding@gmail.com>
2019-02-18 17:37:37 -0500

    done done done donneeee

---
+++ Ding, Jason/chapter13/p13_6/FindLargest.java
@@ -0,0 +1,24 @@
+package p13_6;
+
+public class FindLargest
+{
+	public static void main(String[] args)
+	{
+		int[] array = {4,1,7,1,0,9,7};
+		System.out.println(findLargest(array));
+	}
+	
+	public static int findLargest(int[] a)
+	{
+		return findLargest(a, a.length - 1);
+	}
+	
+	private static int findLargest(int[] a, int start)
+	{
+		if (a.length == 0 || start <= 0)
+			return 0;
+		if (a.length == 1)
+			return a[0];
+		return Math.max(a[start], findLargest(a, start - 1));
+	}
+}
