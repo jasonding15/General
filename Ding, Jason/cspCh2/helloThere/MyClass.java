17bf706404802fc87423150ac4a3d47a43cabb35
jasonding <jasonding@192.168.1.36>
2020-10-23 11:49:20 -0400

    CSP is now in repo that Mr.Horn can access

--- Ding, Jason/cspCh2/helloThere/MyClass.java
+++
@@ -1,9 +0,0 @@
-package helloThere;
-
-public class MyClass {
-
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello there");
-	}
-}
--- Ding, Jason/cspCh2/programmingChallenge/PersonalInfo.java
+++
@@ -1,9 +0,0 @@
-package programmingChallenge;
-
-public class PersonalInfo {
-
-	public static void main(String[] args) {
-		System.out.println("Jason Ding \n8 Dogwood Lane, Boonton Township, NJ 07005 \n9735678396\nUndecided");
-	}
-
-}
